4cb3fd9219296c1817f2b6d760fe2a983cf5946e
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-10 23:54:58 +0000

    Update webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -101,10 +101,7 @@ def checkout(request):
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
+        intent = stripe.PaymentIntent.create(amount=stripe_total, currency=settings.STRIPE_CURRENCY,)
 
         # Attempt to prefill the form with any info the user maintains in their profile
         if request.user.is_authenticated:
