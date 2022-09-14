ca0cfa436fcd11d50a750389173f8b4c14a69f7f
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-14 20:36:32 +0000

    Fix email host env

--- boutique/settings.py
+++ boutique/settings.py
@@ -223,6 +223,6 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environment.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
